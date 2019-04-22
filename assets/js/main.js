6bb627e83d7c9cacc303d780866dcf42c8a5088c
Shakil Alam <itxshakiil@gmail.com>
2019-04-22 20:16:49 +0530

    First Commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById('btn');
+const name = document.getElementById('name');
+const avatar = document.getElementById('avatar');
+btn.addEventListener('click', (e) => {
+
+    if (name.value.trim() === '') {
+        name.focus(); 
+    }
+    else{
+        avatar.setAttribute('src' , `https://joeschmoe.io/api/v1/${name.value}`);
+    }
+})
+particlesJS.load('particles-js', './assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
