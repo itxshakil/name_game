2ede26f32bae1f6e17e24a76adb2cdbc2be0e047
Shakil Alam <itxshakiil@gmail.com>
2019-05-14 11:23:31 +0530

    next

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# name_game
+# Name Game App
+## A Basic Game uses Javascript and an API to show a exclusive avatar for Your Name.
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,18 @@
 const btn = document.getElementById('btn');
 const name = document.getElementById('name');
+const gender = document.getElementById('gender');
 const avatar = document.getElementById('avatar');
 btn.addEventListener('click', (e) => {
-
+    e.preventDefaul();
     if (name.value.trim() === '') {
         name.focus(); 
     }
     else{
-        avatar.setAttribute('src' , `https://joeschmoe.io/api/v1/${name.value}`);
+        if(gender.value == 'random'){
+            avatar.setAttribute('src' , `https://joeschmoe.io/api/v1/${name.value}`);
+    }else{
+        avatar.setAttribute('src' , `https://joeschmoe.io/api/v1/${gender.value}/${name.value}`);
+    }
         avatar.setAttribute('alt' , name.value);
     }
 })
