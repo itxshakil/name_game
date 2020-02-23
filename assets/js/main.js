70e1f52f305d23a8d3267433630ef46006ee06d0
Shakil Alam <itxshakiil@gmail.com>
2020-02-23 15:48:43 +0530

    Added Web Share API

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,17 @@ btn.addEventListener('click', (e) => {
         avatar.setAttribute('alt', name.value);
     }
 });
+document.querySelector('.whatsapp').addEventListener('click',(e)=>{
+    e.preventDefault();
+    if (navigator.share) {
+      navigator.share({
+        title: 'name-game.netlify.com/,
+        text: 'Hey Dear Check this awesome site .What your name say about your Personality.',
+        url: 'https://name-game.netlify.com/',
+      })
+        .then(() => console.log('Successful share'))
+        .catch((error) => console.log('Error sharing', error));
+    }else{     
+        window.location.replace(e.target.href);
+    }
+}
